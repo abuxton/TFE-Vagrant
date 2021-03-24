855075d92d8164b4d7c21c5d84792493e5e57511
Adam Buxton <buxton.adam@gmail.com>
2021-03-24 11:48:50 +0000

    comments for context

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -22,13 +22,16 @@ provider "tfe" {
   hostname        = "localhost"
   ssl_skip_verify = true
   // don't commit tokens to VCS (this token is ephemeral so I don't care for this example branch)
+	// admin user individual token, use this to create orgs and org tokens
   //token           = "TQPZLxNBKMlyaA.atlasv1.9smxOy1pF2pPYFLT2ORsxMzqVh66z2ycQiZzx3rkCsUsVLNZrkdeLzrVwYNGBz1kO7M"
   token = var.tfe_admin_token
 }
 provider "tfe" {
+  // alias = "tfe-org"
   hostname        = "localhost"
   ssl_skip_verify = true
   // don't commit tokens to VCS (this token is ephemeral so I don't care for this example branch)
+	// uses a organisation token created using the admin alias
   //token = "biNPdXJ0MjeviA.atlasv1.zXmSPICB4Yu99TsidebSzlkZBMrzFkLZ0Vpwa1LxeAeKpmzC5jjKPhy7mAFLbzO6ft8"
   token = var.tfe_token
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,5 @@
 variable "tfe_admin_token" {
+  // admin user individual token
   type      = string
   default   = "TQPZLxNBKMlyaA.atlasv1.9smxOy1pF2pPYFLT2ORsxMzqVh66z2ycQiZzx3rkCsUsVLNZrkdeLzrVwYNGBz1kO7M"
   sensative = true
